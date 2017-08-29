3c36d5d4792e55eaf6660934624221aebab7d50b
Darshan Modh <darshanmodh@gmail.com>
2017-08-28 21:52:44 -0700

    added announce method

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3001);
+server.listen();
 
 server.on('listening', function() {
     console.log(`Iris time service is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const announce = () => {
+        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res) => {
+            if(err) {
+                console.log(err);
+                console.log("Error connecting to Iris");
+            }
+        });
+    };
+
+    announce();
+    setInterval(announce, 15*1000);
 });
